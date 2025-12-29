b6e3ff738ced486fb7af8fc3e3a8a9c2c77641a0
Rinat Sarmuldin <ura07srr@gmail.com>
2025-12-29 12:38:29 +0500

    Deleted Trash

--- src/Entity/CustomField.php
+++ src/Entity/CustomField.php
@@ -12,38 +12,69 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(
     name: 'custom_fields',
     uniqueConstraints: [
+        /**
+         * ВАЖНО:
+         * Имя unique должно совпадать с тем, что реально есть/ожидается в БД.
+         * Судя по твоему dump-sql, в БД индекс/constraint называется:
+         *   uniq_custom_fields_position
+         * поэтому фиксируем именно это имя, иначе doctrine:schema:validate будет ругаться
+         * и migrations:diff будет пытаться "дропать/создавать" заново.
+         */
         new ORM\UniqueConstraint(
-            name: 'uniq_inventory_position',
+            name: 'uniq_custom_fields_position',
             columns: ['inventory_id', 'position']
-        )
+        ),
+    ],
+    indexes: [
+        /**
+         * Индекс на FK inventory_id почти всегда есть (и полезен по производительности).
+         * Опять же: фиксируем имя, чтобы Doctrine не пытался переименовывать в IDX_********.
+         */
+        new ORM\Index(
+            name: 'idx_custom_fields_inventory',
+            columns: ['inventory_id']
+        ),
     ]
 )]
 class CustomField
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue] // В Postgres нормально работает с IDENTITY/DEFAULT
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')] // PostgreSQL выдаёт id через IDENTITY
     #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
+    /**
+     * Owning side.
+     * nullable=false + onDelete=CASCADE => поле удаляется вместе с инвентарём.
+     */
     #[ORM\ManyToOne(targetEntity: Inventory::class, inversedBy: 'customFields')]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     private Inventory $inventory;
 
+    /**
+     * Храним строку в БД, наружу отдаём enum.
+     */
     #[ORM\Column(type: 'string', length: 20)]
     private string $type;
 
     #[ORM\Column(type: 'integer')]
     private int $position;
 
-    #[ORM\Column(type: 'boolean')]
+    #[ORM\Column(name: 'is_required', type: 'boolean', options: ['default' => false])]
     private bool $isRequired = false;
 
+    /**
+     * Обязательные поля задаём через constructor,
+     * чтобы сущность не могла существовать в “полусобранном” состоянии.
+     */
     public function __construct(Inventory $inventory, CustomFieldType $type, int $position)
     {
         $this->inventory = $inventory;
         $this->type = $type->value;
         $this->position = $position;
     }
+
+    // ---------------- getters / setters ----------------
 
     public function getId(): ?int
     {
@@ -55,11 +86,11 @@ class CustomField
         return $this->inventory;
     }
 
-    public function setInventory(Inventory $inventory): self
-    {
-        $this->inventory = $inventory;
-        return $this;
-    }
+    /**
+     * Если тебе нужно менять inventory у поля (обычно не нужно),
+     * то можно добавить setInventory(). Сейчас оставляю без него намеренно
+     * (меньше риска “перетаскивать” поля между inventory).
+     */
 
     public function getType(): CustomFieldType
     {
@@ -83,6 +114,11 @@ class CustomField
         return $this;
     }
 
+    /**
+     * Twig:
+     * - f.isRequired  -> вызовет isRequired()
+     * - f.required    -> вызовет isRequired() (из-за правила get/is/has)
+     */
     public function isRequired(): bool
     {
         return $this->isRequired;

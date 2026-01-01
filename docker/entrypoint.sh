7c9d81b6a9fadc572c4cb71fd1375ad56f852b55
Rinat Sarmuldin <ura07srr@gmail.com>
2026-01-01 17:31:04 +0500

    Update readme ind fix compose fot tests

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,6 +7,12 @@ until pg_isready -h database -U app; do
 done
 
 if [ "$APP_ENV" != "prod" ]; then
+  echo "Checking database..."
+  if [ "$APP_ENV" = "test" ]; then
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h database -U app -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'app_test'" | grep -q 1 || \
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h database -U app -d postgres -c "CREATE DATABASE app_test;"
+  fi
+
   echo "Running migrations ($APP_ENV)..."
   php bin/console doctrine:migrations:migrate --no-interaction
 
@@ -15,4 +21,3 @@ if [ "$APP_ENV" != "prod" ]; then
 fi
 
 exec "$@"
-ы
